cb15eb8c01c238a86c2fe95d39f828207835c3d2
JD <jdgramajo@gmail.com>
2022-06-27 19:24:51 -0600

    Adds functionality and tests.

--- sys-info.js
+++ sys-info.js
@@ -1,19 +1,86 @@
 "use strict"
 
-const interactive = () => {
-	console.log("interactive mode entered, rest of params ignored")
-	process.stdin.pipe(process.stdout)
+const os = require('os')
+const { spawnSync } = require('child_process');
+const { statSync } = require('fs');
+
+const interactiveHelpObject = {
+	title: "Help",
+	message: "Available commands: help, system, user, directory, details, exit"
+}
+
+const scriptHelpObject = {
+	title: "Usage",
+	message: "node [command] # any of: help, system, user, directory, details, exit"
+}
+
+const getSystemInfoObject = () => {
+	return {
+		type: os.type(),
+		architecture: os.arch(),
+		hostname: os.hostname(),
+		platform: os.platform(),
+		cpus: os.cpus().length,
+		totalmem: `${os.totalmem()/(1024*1024*1024)}GB`,
+		uptime: os.uptime()
+	}
+}
+
+const getUserInfoObject = () => os.userInfo()
+
+const getFilteredFileListObject = () => {
+	const commonlyIgnoredFileNames = [",node_modules", ".", ","] // Quick fix
+	const rawFileList = spawnSync("ls").output.toString().split('\n')
+	return rawFileList.filter(filename => !commonlyIgnoredFileNames.includes(filename))
+}
+
+const getFileStats = (name) => {
+	try {
+		const stats = statSync(name)
+		return stats
+	} catch(error) {
+		console.log("Something went wrong, double check with \"directory\"")
+	}
+}
+
+const commandProcessor = (command) => {
+	switch (command[0]) {
+		case "help":
+			console.log(JSON.stringify(interactiveHelpObject))
+			break
+		case "system":
+			console.log(JSON.stringify(getSystemInfoObject()))
+			break
+		case "user":
+			console.log(JSON.stringify(getUserInfoObject()))
+			break
+		case "directory":
+			console.log(JSON.stringify(getFilteredFileListObject()))
+			break
+		case "details":
+			console.log(JSON.stringify(getFileStats(command[1])))
+			break
+		case "exit":
+			process.exit(0)
+		default:
+			console.log("unrecognized command")
+			break
+	}
 }
 
 const processParams = (params) => {
-	console.log(params)
-	if (params[0] === "-i") interactive()
+	if (params[0] === "-i") {
+		console.log("Entered the interactive mode, rest of params will be ignored.")
+		process.stdin.on("data", data => commandProcessor(data.toString().trim().split(' ')))
+		return
+	}
+	commandProcessor(params)
 }
 
 const run = () => {
-	const argvCount = process.argv.length - 2;
+	const argvCount = process.argv.length - 2
 	if (argvCount < 1) {
-		console.log(`usage:\n\n\tsome instructions are here`)
+		console.log(JSON.stringify(scriptHelpObject))
 		// TODO: add tests for exit status
 		process.exit(1)
 	}
@@ -21,4 +88,3 @@ const run = () => {
 }
 
 run()
-
--- test/scope.test.js
+++ test/scope.test.js
@@ -1,18 +1,29 @@
 "use strict"
 
+const { spawn } = require("child_process");
 const tap = require("tap")
 
-tap.test("can run as script", t => {
-	t.todo("shows a message starting with usage: if no params are passed")
+tap.test("script mode tests", t => {
+	t.test("given an empty command, shows help message that contains \"Usage\" and exits", t => {
+		const child = spawn("node", ["../sys-info.js"], { cwd: __dirname });
+		child.stdout.on("data", data => t.match(data.toString(), /Usage/, "must show usage message"))
+		child.on("close", () => t.end())
+	})
+	t.test("given good command, outputs dir data and exits", t => {
+		const child = spawn("node", ["../sys-info.js", "directory"], { cwd: __dirname });
+		child.stdout.on("data", data => t.doesNotThrow(() => JSON.parse(data.toString()).length, 
+			"output should parse to array of strings with dir contents"))
+		child.on("close", () => t.end())
+	})
+	t.test("given a bad command, \"unrecognized command\" is output and process exits", t => {
+		const child = spawn("node", ["../sys-info.js", "bla"], { cwd: __dirname });
+		child.stdout.on("data", data => t.match(data.toString(), /unrecognized command/, "must indicate bad usage"))
+		child.on("close", () => t.end())
+	})
+	t.end()
 })
 
-tap.test("can parse and process params", t => {
-	t.test("when params are passed, they get stored to an array")
-	t.test("a FIRST -i param will trigger interactive mode (event logger???)")
-})
-
-tap.test("can capture input when interactive", t => {
-	t.todo("pipes stdin and saves commands to an array, displays arrays contents")
-})
-
-
+// tap.test("interactive mode tests", t => {
+// 	t.test("given \"-i\" flag, interactive mode is launched")
+// 	t.test("given good command, output buffer gets data")
+// })
