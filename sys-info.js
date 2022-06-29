bcc06e5521371b0588eea8e27f6dc3d704c34ea5
JD <jdgramajo@gmail.com>
2022-06-28 22:23:24 -0600

    Deprecates using spawning process, improves and fixes tests to ensure there is process output, adds tdd tests for a display command

--- sys-info.js
+++ sys-info.js
@@ -1,17 +1,19 @@
 "use strict"
 
 const os = require('os')
-const { spawnSync } = require('child_process');
-const { statSync } = require('fs');
+const { statSync, readdirSync, mkdirSync, writeFileSync } = require('fs')
 
 const interactiveHelpObject = {
 	title: "Help",
-	message: "Available commands: help, system, user, directory, details, exit"
+	message: "Available commands: help, system, user, directory, details {file}, exit"
+		+ "\n\n-s as last param to save output to a file under the info directory"
 }
 
 const scriptHelpObject = {
 	title: "Usage",
-	message: "node [command] # any of: help, system, user, directory, details, exit"
+	message: "node [command] # any of: help, system, user, directory, details {file}, exit"
+	  + "\n\n-i for interactive mode, will ignore the rest of them"
+	  + "\n\n-s as last param to save output to a file under the info directory"
 }
 
 const getSystemInfoObject = () => {
@@ -29,9 +31,11 @@ const getSystemInfoObject = () => {
 const getUserInfoObject = () => os.userInfo()
 
 const getFilteredFileListObject = () => {
-	const commonlyIgnoredFileNames = [",node_modules", ".", ","] // Quick fix
-	const rawFileList = spawnSync("ls").output.toString().split('\n')
-	return rawFileList.filter(filename => !commonlyIgnoredFileNames.includes(filename))
+	const commonlyIgnoredFileNames = [ "node_modules", ".git", ".nyc_output" ] // Quick fix
+	const rawFileList = readdirSync(__dirname)
+
+	return rawFileList.filter(filename => !commonlyIgnoredFileNames.includes(filename)
+		&& !__filename.includes(filename))
 }
 
 const getFileStats = (name) => {
@@ -44,28 +48,39 @@ const getFileStats = (name) => {
 }
 
 const commandProcessor = (command) => {
+
+	let commandOutput = ""
+
 	switch (command[0]) {
 		case "help":
-			console.log(JSON.stringify(interactiveHelpObject))
+			commandOutput = JSON.stringify(interactiveHelpObject)
 			break
 		case "system":
-			console.log(JSON.stringify(getSystemInfoObject()))
+			commandOutput = JSON.stringify(getSystemInfoObject())
 			break
 		case "user":
-			console.log(JSON.stringify(getUserInfoObject()))
+			commandOutput = JSON.stringify(getUserInfoObject())
 			break
 		case "directory":
-			console.log(JSON.stringify(getFilteredFileListObject()))
+			commandOutput = JSON.stringify(getFilteredFileListObject())
 			break
 		case "details":
-			console.log(JSON.stringify(getFileStats(command[1])))
+			commandOutput = JSON.stringify(getFileStats(command[1]))
 			break
 		case "exit":
 			process.exit(0)
 		default:
-			console.log("unrecognized command")
+			commandOutput = "unrecognized command"
 			break
 	}
+
+	if (command.slice(-1).toString() === "-s") {
+		mkdirSync("./info")
+		writeFileSync(`./info/${command[0]}.json`, commandOutput)
+		commandOutput = `./info/${command[0]}.json file created/updated`
+	}
+
+	console.log(commandOutput)
 }
 
 const processParams = (params) => {
@@ -74,16 +89,20 @@ const processParams = (params) => {
 		process.stdin.on("data", data => commandProcessor(data.toString().trim().split(' ')))
 		return
 	}
+
 	commandProcessor(params)
 }
 
 const run = () => {
+
 	const argvCount = process.argv.length - 2
+
 	if (argvCount < 1) {
 		console.log(JSON.stringify(scriptHelpObject))
 		// TODO: add tests for exit status
 		process.exit(1)
 	}
+
 	processParams(process.argv.slice(2, process.argv.length))
 }
 
--- test/scope.test.js
+++ test/scope.test.js
@@ -2,31 +2,111 @@
 
 const { spawn } = require("child_process");
 const tap = require("tap")
-const stream = require("stream")
+const fs = require("fs")
+const { join } = require("path")
 
 tap.test("script mode tests", t => {
+
 	t.test("given an empty command", t => {
-		const child = spawn("node", ["../sys-info.js"], { cwd: __dirname })
-		child.stdout.on("data", data => t.match(data.toString(), /Usage/, "must show usage message"))
-		child.on("close", () => t.end())
+
+		let dataEventEmitted = false
+		const child = spawn("node", ["sys-info.js"], { cwd: join(__dirname, '..') })
+
+		child.stdout.on("data", data => {
+			dataEventEmitted = true
+			console.log(data.toString())
+			t.match(data.toString(), /(title|Usage|message)/, "must show usage message")
+		})
+
+		child.on("close", () => {
+			t.ok(dataEventEmitted, "stdout should emit data")
+			t.end()
+		})
 	})
+
 	t.test("given good command", t => {
-		const child = spawn("node", ["../sys-info.js", "directory"], { cwd: __dirname })
-		child.stdout.on("data", data => t.doesNotThrow(() => JSON.parse(data.toString()).length, 
-			"output should parse to an array of strings with dir contents"))
-		child.on("close", () => t.end())
+
+		let dataEventEmitted = false
+		const child = spawn("node", ["sys-info.js", "directory"], { cwd: join(__dirname, '..') })
+
+		child.stdout.on("data", data => {
+			dataEventEmitted = true
+			console.log(data.toString())
+			t.same(typeof JSON.parse(data.toString()), "object",
+				"output should parse to a valid JSON instance")
+		})
+
+		child.on("close", () => {
+			t.ok(dataEventEmitted, "stdout should emit data")
+			t.end()
+		})
 	})
+
 	t.test("given a bad command", t => {
-		const child = spawn("node", ["../sys-info.js", "bla"], { cwd: __dirname })
-		child.stdout.on("data", data => t.match(data.toString(), /unrecognized command/, "must indicate bad usage"))
-		child.on("close", () => t.end())
+
+		let dataEventEmitted = false
+		const child = spawn("node", ["sys-info.js", "bla"], { cwd: join(__dirname, "..") })
+
+		child.stdout.on("data", data => {
+			dataEventEmitted = true
+			t.match(data.toString(), /unrecognized command/, "must indicate bad usage")
+		})
+
+		child.on("close", () => {
+			t.ok(dataEventEmitted, "stdout should emit data")
+			t.end()
+		})
 	})
+
+	t.test("when outputing commands end with the \"-s\" flag", t => {
+
+		let dataEventEmitted = false
+		const child = spawn("node", ["sys-info.js", "system", "-s"], { cwd: join(__dirname, "..") })
+
+		child.stdout.on("data", data => {
+			dataEventEmitted = true
+			t.doesNotThrow(() => fs.readdirSync("info"), "info directory must exist or have been created")
+			t.doesNotThrow(() => fs.readFileSync("info/system.json"), "must create output file, with the name of the command")
+			t.match(data.toString(), /(system|created|updated)/, "must output that file named as the command was created")
+		})
+
+		child.on("close", () => {
+			t.ok(dataEventEmitted, "stdout should emit data")
+			t.end()
+		})
+	})
+
+	t.test("\"display\" command tests", t => {
+		t.test("with a valid option specified", () => {
+
+			let dataEventEmitted = false
+			const child = spawn("node", ["sys-info.js", "display", "system"], { cwd: join(__dirname, "..") })
+
+			child.stdout.on("data", data => {
+				dataEventEmitted = true
+				t.same(data.toString(), fs.readFileSync("info/system.json").toString(), "must output file contents")
+			})
+
+			child.on("close", () => {
+				t.ok(dataEventEmitted, "stdout should emit data")
+				t.end()
+			})
+		})
+
+		t.todo("with a invalid file specified, it outputs an error message")
+		t.todo("with no additional params, it displays all existing files")
+		t.end()
+	})
+
 	t.end()
+
 })
 
 tap.test("interactive mode tests", t => {
+
 	t.test("given \"-i\" flag, interactive mode is launched, within it:", t => {
-		const child = spawn("node", ["../sys-info.js", "-i"], { cwd: __dirname })
+
+		const child = spawn("node", ["sys-info.js", "-i"], { cwd: join(__dirname, "..") })
 		const commandTests = [
 			{ genExp: /(atime|mtime|ctime|birthtime)/, message: "must output file stats", nextCommand: "exit\n" },
 			{ genExp: /(error|hint)/, message: "must output error message & hint", nextCommand: `details ${__filename}\n` },
@@ -36,13 +116,26 @@ tap.test("interactive mode tests", t => {
 			{ genExp: /(title|message)/, message: "must dispay help", nextCommand: "system\n" },
 			{ genExp: /interactive mode/, message: "must enter interactive mode", nextCommand: "help\n" }
 		]
+
+		// First output does not count, but last call should. Balanced, as everything should be.
+		const commandTestCount = commandTests.length
+		let childDataEventCount = 0
+
 		child.stdout.on("data", data => {
+			childDataEventCount++
 			const { genExp, message, nextCommand } = commandTests.pop()
 			t.match(data.toString(), genExp, message)
 			child.stdin.write(nextCommand)
 		})
-		child.on("close", () => t.end())
+
+		child.on("close", () => {
+			t.ok(childDataEventCount === commandTestCount,
+				`child process should output ${commandTests.length} times, did ${childDataEventCount}`)
+			t.end()
+		})
 	})
+
 	t.end()
+
 })
 
